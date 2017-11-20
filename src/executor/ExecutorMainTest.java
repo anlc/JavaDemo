a5247974b590860f74cc24bb44b57776a44b542d
anlaochou <anlaochou@iweichan.com>
2017-11-20 13:59:58 +0800

    change name

--- src/executor/ExecutorMainTest.java
+++ src/executor/ExecutorMainTest.java
@@ -3,20 +3,21 @@ package executor;
 /**
  * Created by n on 2017/11/20.
  */
-public class ExecutorTest {
+public class ExecutorMainTest {
 
     public static void main(String[] args) {
-        ExecutorTest test = new ExecutorTest();
+        ExecutorMainTest test = new ExecutorMainTest();
 
-        test.test("cache");
+//        test.test("cache");
 //        test.test("fixed");
-//        test.test("single");
+        test.test("single");
     }
 
     public void test(String type) {
         ExecutorDemo demo = new ExecutorDemo(type);
         for (int i = 0; i < 5; i++) {
-            demo.execute(new RunnableClass());
+//            demo.execute(new RunnableClass());
+            demo.submit(new RunnableClass());
         }
         demo.release();
     }
