f7ebc0cb0ebb5266b1c8dddd650e9359941ec226
anlaochou@iweichan.com <anlaochou@iweichan.com>
2018-03-06 14:24:19 +0800

    add state pattern

---
+++ src/pattern/design_pattern/StatePattern.java
@@ -0,0 +1,57 @@
+package pattern.design_pattern;
+
+/**
+ * 状态模式
+ * 参考：
+ * http://www.runoob.com/design-pattern/state-pattern.html
+ */
+public class StatePattern {
+
+    static class Context{
+        public State state;
+    }
+
+    interface State{
+        void doAction(Context context);
+    }
+
+    static class StartState implements State{
+
+        @Override
+        public void doAction(Context context) {
+            System.out.println("player is in start state");
+            context.state = this;
+        }
+
+        @Override
+        public String toString() {
+            return "start state";
+        }
+    }
+
+    static class StopState implements State{
+
+        @Override
+        public void doAction(Context context) {
+            System.out.println("player is in stop state");
+            context.state = this;
+        }
+
+        @Override
+        public String toString() {
+            return "stop state";
+        }
+    }
+
+    public static void main(String[] args) {
+        Context context = new Context();
+
+        StartState startState = new StartState();
+        startState.doAction(context);
+        System.out.println(context.state.toString());
+
+        StopState stopState = new StopState();
+        stopState.doAction(context);
+        System.out.println(context.state.toString());
+    }
+}
