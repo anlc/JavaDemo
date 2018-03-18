184c783bd28ff1ea5ccfbcc0e0177d6681ea208c
anlaochou <anlaochou@iweichan.com>
2018-03-18 22:09:42 +0800

    add iterator pattern

---
+++ src/pattern/design_pattern/IteratorPattern.java
@@ -0,0 +1,57 @@
+package pattern.design_pattern;
+
+/**
+ * Created by n on 2018/3/18.
+ * 参考：
+ * http://www.runoob.com/design-pattern/iterator-pattern.html
+ */
+public class IteratorPattern {
+
+    interface Iterator {
+        boolean hasNext();
+
+        Object next();
+    }
+
+    interface Container {
+        Iterator getIterator();
+    }
+
+    static class NameRepository implements Container {
+        public String names[] = {"123", "321", "789"};
+
+        @Override
+        public Iterator getIterator() {
+            return new NameIterator();
+        }
+
+        class NameIterator implements Iterator {
+
+            int index;
+
+            @Override
+            public boolean hasNext() {
+                if (index < names.length) {
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public Object next() {
+                if (hasNext()) {
+                    return names[index++];
+                }
+                return null;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        NameRepository repository = new NameRepository();
+        Iterator iterator = repository.getIterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
