b138371896fe6485e7538c641906102ea05ca9d1
anlaochou@iweichan.com <494243786@qq.com>
2018-03-27 17:16:27 +0800

    add builder pattern

---
+++ src/pattern/design_pattern/BuilderPattern.java
@@ -0,0 +1,52 @@
+package pattern.design_pattern;
+
+import org.junit.Test;
+
+/**
+ * 建造者模式
+ */
+public class BuilderPattern {
+
+    static class Dialog {
+
+        Build build;
+
+        private Dialog(Build build) {
+            this.build = build;
+        }
+
+        public void show() {
+            System.out.println("msg:" + build.msg + "  title:" + build.title);
+        }
+
+        static class Build {
+            String msg;
+            String title;
+
+            public Build() {
+                msg = "default msg";
+                title = "default title";
+            }
+
+            public Build msg(String msg) {
+                this.msg = msg;
+                return this;
+            }
+
+            public Build title(String title) {
+                this.title = title;
+                return this;
+            }
+
+            Dialog build() {
+                return new Dialog(this);
+            }
+        }
+    }
+
+    @Test
+    public void testPattern() {
+        new Dialog.Build().build().show();
+        new Dialog.Build().title("new title").build().show();
+    }
+}
