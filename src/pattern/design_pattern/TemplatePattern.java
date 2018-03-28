67e1952ad8c7484a2fd30e850aa4b3981b77bef3
anlaochou@iweichan.com <494243786@qq.com>
2018-03-28 17:21:32 +0800

    add template pattern

---
+++ src/pattern/design_pattern/TemplatePattern.java
@@ -0,0 +1,70 @@
+package pattern.design_pattern;
+
+import org.junit.Test;
+
+/**
+ * 模板模式
+ *
+ * 参考：http://www.runoob.com/design-pattern/template-pattern.html
+ * 源码参考：AsyncTask
+ */
+public class TemplatePattern {
+
+    abstract class Game {
+        abstract void init();
+
+        abstract void startPlay();
+
+        abstract void endPlay();
+
+        public final void play() {
+            init();
+            startPlay();
+            endPlay();
+        }
+    }
+
+    class Cricket extends Game {
+
+        @Override
+        void init() {
+            System.out.println("Cricket Game Finished!");
+        }
+
+        @Override
+        void startPlay() {
+            System.out.println("Cricket Game Initialized! Start playing.");
+        }
+
+        @Override
+        void endPlay() {
+            System.out.println("Cricket Game Started. Enjoy the game!");
+        }
+    }
+
+    class FootBall extends Game {
+
+        @Override
+        void init() {
+            System.out.println("Football Game Finished!");
+        }
+
+        @Override
+        void startPlay() {
+            System.out.println("Football Game Initialized! Start playing.");
+        }
+
+        @Override
+        void endPlay() {
+            System.out.println("Football Game Started. Enjoy the game!");
+        }
+    }
+
+    @Test
+    public void testPattern() {
+        new Cricket().play();
+        System.out.println();
+        new FootBall().play();
+    }
+
+}
