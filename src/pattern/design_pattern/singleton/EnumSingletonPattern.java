a60416d41005394d6c5ab4968cae1e06e2b38895
anlaochou@iweichan.com <494243786@qq.com>
2018-03-29 08:59:44 +0800

    add enum singleton pattern

---
+++ src/pattern/design_pattern/singleton/EnumSingletonPattern.java
@@ -0,0 +1,27 @@
+package pattern.design_pattern.singleton;
+
+import org.junit.Test;
+
+/**
+ * 单例模式
+ * 枚举
+ *
+ * 参考：http://www.runoob.com/design-pattern/singleton-pattern.html
+ */
+public class EnumSingletonPattern {
+
+    enum Singleton {
+
+        INSTANCE;
+
+        @Override
+        public String toString() {
+            return "enum singleton pattern";
+        }
+    }
+
+    @Test
+    public void testPattern() {
+        System.out.println(Singleton.INSTANCE.toString());
+    }
+}
