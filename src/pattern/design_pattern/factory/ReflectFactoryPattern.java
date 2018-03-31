6b9aefd7bdc74c4587b4b6a24e9e874ba573077c
anlaochou <494243786@qq.com>
2018-03-31 14:17:25 +0800

    add factory pattern

---
+++ src/pattern/design_pattern/factory/ReflectFactoryPattern.java
@@ -0,0 +1,43 @@
+package pattern.design_pattern.factory;
+
+import org.junit.Test;
+
+/**
+ * 反射的方式创建对象
+ */
+public class ReflectFactoryPattern {
+
+    interface Shape {
+    }
+
+    static class Circle implements Shape {
+        @Override
+        public String toString() {
+            return "create circle";
+        }
+    }
+
+    static class Square implements Shape {
+        @Override
+        public String toString() {
+            return "create square";
+        }
+    }
+
+    static class Factory {
+        public static <T extends Shape> T crate(Class<T> clazz) {
+            try {
+                return clazz.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    @Test
+    public void testPattern() {
+        System.out.println(Factory.crate(Circle.class));
+        System.out.println(Factory.crate(Square.class));
+    }
+}
---
+++ src/pattern/design_pattern/factory/RegisterFactoryPattern.java
@@ -0,0 +1,63 @@
+package pattern.design_pattern.factory;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 注册工厂模式
+ *
+ * 参考：Java 编程思想  582页
+ */
+public class RegisterFactoryPattern {
+
+    interface Factory<T> {
+        T create();
+    }
+
+    static class Part {
+        @Override
+        public String toString() {
+            return getClass().getSimpleName();
+        }
+
+        static List<Factory<? extends Part>> partFactories = new ArrayList<>();
+
+        static {
+            partFactories.add(new FuelFilter.Factory());
+            partFactories.add(FanBelt::new);
+        }
+
+        public static Part create(int position) {
+            return partFactories.get(position).create();
+        }
+    }
+
+    static class Filter extends Part {}
+
+    static class FuelFilter extends Filter {
+        public static class Factory implements RegisterFactoryPattern.Factory<FuelFilter> {
+            @Override
+            public FuelFilter create() {
+                return new FuelFilter();
+            }
+        }
+    }
+
+    static class Belt extends Part {}
+
+    static class FanBelt extends Belt implements Factory<FanBelt> {
+        @Override
+        public FanBelt create() {
+            return new FanBelt();
+        }
+    }
+
+    @Test
+    public void testPattern() {
+        for (int i = 0; i < Part.partFactories.size(); i++) {
+            System.out.println(Part.create(i));
+        }
+    }
+}
