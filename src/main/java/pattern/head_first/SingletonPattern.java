24575f3cfc5a95832430e8a0e496b69471d092a9
anlc <494243786@qq.com>
2022-01-20 21:10:23 +0800

    refactor: doc

---
+++ src/main/java/pattern/head_first/SingletonPattern.java
@@ -0,0 +1,36 @@
+package pattern.head_first;
+
+public class SingletonPattern {
+
+    public static final class Singleton {
+
+        private volatile static Singleton INSTANCE;
+
+        /**
+         * 将构造方法私有化
+         */
+        private Singleton() {}
+
+        /**
+         * 提供一个静态方法获取实例对象
+         */
+        private static Singleton getInstance() {
+            if (INSTANCE == null) {
+                synchronized (Singleton.class) {
+                    if (INSTANCE == null) {
+                        INSTANCE = new Singleton();
+                    }
+                }
+            }
+            return INSTANCE;
+        }
+
+        public void display(){
+            System.out.println("display");
+        }
+    }
+
+    public static void main(String[] args) {
+        Singleton.getInstance().display();
+    }
+}
